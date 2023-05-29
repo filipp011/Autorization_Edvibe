6e6c43a60732499fbae421ddfcb1c827c46335f6
Filipp Nis <fil.nis61@gmail.com>
2023-05-29 17:04:58 +0300

    Autotest authorization - done

---
+++ lesson_5.py
@@ -0,0 +1,16 @@
+from selene import browser
+from selene import be, have,by
+
+def test_lesson_5():
+    browser.open("https://edvibe.com/Account/Login")
+    browser.driver.maximize_window()
+    browser.element('[name="Email"]').should(be.blank).type('43@mail.ru').press_enter()
+    browser.element('[name="Password"]').should(be.blank).type('liveUT00mPE8CB7Z').press_enter()
+    # browser.element('[type="button"]').
+    #browser.element("/html/body/div[1]/div/div[2]/form/button[2]").click()
+    #by.xpath("/html/body/div[1]/div/div[2]/form/button[2]")
+    browser.element(by.xpath("/html/body/div[1]/div/div[2]/form/button[2]")).click()
+    ...
+
+
+
